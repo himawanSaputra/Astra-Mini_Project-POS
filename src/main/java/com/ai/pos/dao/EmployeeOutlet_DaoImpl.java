3ba4f66d899e04f2ac108c7e3cf6c55a5ac33fe4
William Roridande Aphrodita <31577462+roridande@users.noreply.github.com>
2019-11-22 16:51:18 +0700

    Add Dao Willi

---
+++ src/main/java/com/ai/pos/dao/EmployeeOutlet_DaoImpl.java
@@ -0,0 +1,35 @@
+package com.ai.pos.dao;
+
+
+import com.ai.pos.model.EmployeeOutlet;
+
+import java.util.List;
+
+public class EmployeeOutlet_DaoImpl implements EmployeeOutlet_Dao{
+
+
+    @Override
+    public void addEmployeeOutlet(EmployeeOutlet employeeOutlet) {
+
+    }
+
+    @Override
+    public void updateEmployeeOutlet(EmployeeOutlet employeeOutlet) {
+
+    }
+
+    @Override
+    public void deleteEmployeeOutlet(EmployeeOutlet employeeOutlet) {
+
+    }
+
+    @Override
+    public EmployeeOutlet findOne(int id) {
+        return null;
+    }
+
+    @Override
+    public List<EmployeeOutlet> employeeOutlet() {
+        return null;
+    }
+}
