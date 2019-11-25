58e3b8261d49a2ba2f56c1f2601d91002534f824
Armandharma <armandarma96@gmail.com>
2019-11-25 09:38:58 +0700

    service category

---
+++ src/main/java/com/ai/pos/service/MstItemService.java
@@ -0,0 +1,37 @@
+package com.ai.pos.service;
+
+import com.ai.pos.dao.MstItemDao;
+import com.ai.pos.model.MstItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class MstItemService {
+
+    @Autowired
+    private MstItemDao mstItemDao;
+
+    public void save(MstItem mstItem){
+        mstItemDao.save(mstItem);
+    }
+
+    public void update(MstItem mstItem){
+        mstItemDao.update(mstItem);
+    }
+
+    public void delete(MstItem mstItem){
+        mstItemDao.delete(mstItem);
+    }
+
+    public MstItem getItem(int id){
+        return mstItemDao.getItem(id);
+    }
+
+    public List<MstItem> getAllItem(){
+        return mstItemDao.getAllItem();
+    }
+}
