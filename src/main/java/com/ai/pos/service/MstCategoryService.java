dc472628e1c720d9515022e1bd9175ace899b853
Armandharma <armandarma96@gmail.com>
2019-11-25 11:08:47 +0700

    commit1108

---
+++ src/main/java/com/ai/pos/service/MstCategoryService.java
@@ -0,0 +1,13 @@
+package com.ai.pos.service;
+
+import com.ai.pos.model.MstCategory;
+
+import java.util.List;
+
+public interface MstCategoryService {
+    public void save(MstCategory mstCategory);
+    public void update(MstCategory mstCategory);
+    public void delete(MstCategory mstCategory);
+    public MstCategory getCategory(int id);
+    public List<MstCategory> getAllCategory();
+}
