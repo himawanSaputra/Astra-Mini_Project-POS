984fe52413a2d00aded6a6fb7a5e90f746bb40ff
William Roridande Aphrodita <31577462+roridande@users.noreply.github.com>
2019-11-25 10:12:36 +0700

    Add Dao and DaoImpl

--- src/main/java/com/ai/pos/dao/Employee_Dao.java
+++ src/main/java/com/ai/pos/dao/Employee_Dao.java
@@ -8,7 +8,7 @@ public interface Employee_Dao {
 
     public void addEmployee(MstEmployee mstEmployee);
     public void updateEmployee(MstEmployee mstEmployee);
-    public void deleteEmployee(MstEmployee mstEmployee);
+    public void deleteEmployee(int id);
     public MstEmployee findOne(int id);
     public List<MstEmployee> listMstEmployee();
 }
--- src/main/java/com/ai/pos/dao/Role_DaoImpl.java
+++ src/main/java/com/ai/pos/dao/Role_DaoImpl.java
@@ -1,32 +1,57 @@
 package com.ai.pos.dao;
 
 import com.ai.pos.model.MstRole;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import java.util.List;
 
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+
 public class Role_DaoImpl implements Role_Dao {
+
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sf){
+        this.sessionFactory = sf;
+    }
+
     @Override
     public void addRole(MstRole mstRole) {
-
+        Session session = this.sessionFactory.getCurrentSession();
+        session.persist(mstRole);
     }
 
     @Override
     public void updateRole(MstRole mstRole) {
-
+        Session session = this.sessionFactory.getCurrentSession();
+        session.update(mstRole);
     }
 
     @Override
-    public void deleteRole(MstRole mstRole) {
-
+    public void deleteRole(int id) {
+        Session session = this.sessionFactory.getCurrentSession();
+        MstRole mstRole = (MstRole) session.load(MstRole.class, new Integer(id));
+        if(null != mstRole){
+            session.delete(mstRole);
+        }
     }
+
 
     @Override
     public MstRole findOne(int id) {
-        return null;
+        Session session = this.sessionFactory.getCurrentSession();
+        MstRole mstRole = (MstRole) session.load(MstRole.class, new Integer(id));
+        return mstRole;
     }
 
     @Override
     public List<MstRole> listMstRole() {
-        return null;
+        Session session = this.sessionFactory.getCurrentSession();
+        List<MstRole> mstRoleList = session.createQuery("from Role").list();
+        for(MstRole mstRole : mstRoleList){
+            LOGGER.info("Role list:"+mstRole);
+        }
+        return mstRoleList;
     }
 }
